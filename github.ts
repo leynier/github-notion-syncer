244e48f23154d1c26bff725726d971ef94c6b032
Leynier Gutiérrez González <leynier41@gmail.com>
2022-12-23 23:15:30 -0500

    fix: fetch all issues

--- github.ts
+++ github.ts
@@ -2,15 +2,6 @@
 import { Octokit } from "https://cdn.skypack.dev/octokit?dts";
 import { config } from "https://deno.land/x/dotenv@v3.2.0/mod.ts";
 
-const envConfig = config();
-const githubToken = envConfig.GITHUB_TOKEN;
-
-const octokit = new Octokit({ auth: githubToken });
-
-const response = await octokit.rest.search.issuesAndPullRequests({
-  q: "is:open is:issue assignee:leynier archived:false user:educup",
-});
-
 class Assignee {
   username: string;
   url: string;
@@ -49,20 +40,43 @@ class Issue {
   }
 }
 
-const issues: Issue[] = response.data.items.map((item: any) =>
-  new Issue({
-    id: item.id.toString(),
-    title: item.title,
-    repo: item.repository_url.split("/").slice(-1)[0],
-    url: item.html_url,
-    assignees: item.assignees.map((assignee: any) =>
-      new Assignee({
-        username: assignee.login,
-        url: assignee.html_url,
-      })
-    ),
-    labels: item.labels.map((label: any) => label.name),
-  })
-);
+const envConfig = config();
+const githubToken = envConfig.GITHUB_TOKEN;
+
+const octokit = new Octokit({ auth: githubToken });
+
+function getIssues(response: any) {
+  const issues: Issue[] = response.data.items.map((item: any) =>
+    new Issue({
+      id: item.id.toString(),
+      title: item.title,
+      repo: item.repository_url.split("/").slice(-1)[0],
+      url: item.html_url,
+      assignees: item.assignees.map((assignee: any) =>
+        new Assignee({
+          username: assignee.login,
+          url: assignee.html_url,
+        })
+      ),
+      labels: item.labels.map((label: any) => label.name),
+    })
+  );
+  return issues;
+}
+
+let issues: Issue[] = [];
+let page = 1;
+while (true) {
+  const response = await octokit.rest.search.issuesAndPullRequests({
+    q: "is:open is:issue assignee:leynier archived:false user:educup",
+    page: page,
+  });
+  const currentIssues = getIssues(response);
+  if (currentIssues.length === 0) {
+    break;
+  }
+  page++;
+  issues = issues.concat(currentIssues);
+}
 
 export { issues };
